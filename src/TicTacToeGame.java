fb2acd75f15bcc36e0dfff04356889e88c503e99
Naman <cool.naman.ajmera@gmail.com>
2021-04-25 22:34:29 +0530

    Added: Taking input from user.

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class TicTacToeGame {
 	
 	static Scanner sc = new Scanner(System.in);	
+	static char player, computer;
+
 	
 	// UC1 - Creating a Board
 		public static char[] createBoard() {
@@ -13,6 +15,25 @@ public class TicTacToeGame {
 			return board;
 		}
 		
+		// UC2 - Taking Input
+		public static char takeInput() {
+			System.out.println("Enter the letter you wish to take: ('X' / 'O'): ");
+			char input = Character.toUpperCase(sc.next().charAt(0));
+			if (input == 'X' || input == 'O') {
+				setLetter(input);
+				return input;
+			} else {
+				System.out.println("Invalid Character. Try Again.");
+				return takeInput();
+			}
+		}
+
+		// UC2 - Determine letter for player and computer
+		public static void setLetter(char player) {
+			computer = (player == 'X') ? 'O' : 'X';
+			System.out.println("Player Letter: " + player + "\nComputer Letter : " + computer);
+		}
+		
 		private static void startGame() {
 			char[] board = createBoard();
 		}
@@ -20,6 +41,7 @@ public class TicTacToeGame {
 			// TODO Auto-generated method stub
 			System.out.println("Welcome to the TicTacToe Game");
 			startGame();
+			takeInput();
 			sc.close();
 		}
 
